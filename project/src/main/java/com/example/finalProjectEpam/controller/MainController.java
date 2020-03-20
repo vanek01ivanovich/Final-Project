f5f884194d0424041ee9d7a5237993e0c8882384
Ivan Kuchkov <ivan.kuchkov17@gmail.com>
2020-03-20 23:58:11 +0200

    add alerts to sig in

--- project/src/main/java/com/example/finalProjectEpam/controller/MainController.java
+++ project/src/main/java/com/example/finalProjectEpam/controller/MainController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.LocaleResolver;
@@ -53,7 +54,8 @@ public class MainController {
     }
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration(Model model){
+
         return "registration";
     }
 
--- project/src/main/java/com/example/finalProjectEpam/controller/RegistrationController.java
+++ project/src/main/java/com/example/finalProjectEpam/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.example.finalProjectEpam.controller;
 import com.example.finalProjectEpam.entity.User;
 import com.example.finalProjectEpam.entity.enums.RoleStatus;
 import com.example.finalProjectEpam.service.implementation.UserServiceImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping(value = "/registration")
@@ -25,19 +28,26 @@ public class RegistrationController {
     }
 
     @RequestMapping(value = "/save")
-    public ModelAndView saveUser(User user, Model model){
-        ModelAndView modelAndView = new ModelAndView();
+    public ModelAndView saveUser(User user, Model model, RedirectAttributes redirectAttrs){
+        ModelAndView modelAndView;
         user.setRole(RoleStatus.ROLE_USER);
         if(userServiceImpl.existsUserByUserName(user.getUserName())){
             System.out.println("exists");
+            Integer al = 0;
+            redirectAttrs.addFlashAttribute("alert",al);
+
 
             return new ModelAndView("redirect:/registration");
         }else {
             System.out.println("not exist");
-            userServiceImpl.addUser(user);
+            Integer al = 1;
+            redirectAttrs.addFlashAttribute("alert",al);
+            //userServiceImpl.addUser(user);
+
             return new ModelAndView("redirect:/login");
         }
         //
+
 
     }
 
